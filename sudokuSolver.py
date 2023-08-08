9e7c5eab0c1465dc98621dab4ad2b61cb66eaa44
Aditya Ved <63937430+aved2@users.noreply.github.com>
2023-08-07 17:25:10 -0700

    Add files via upload

---
+++ sudokuSolver.py
@@ -0,0 +1,82 @@
+sudoku = [
+    [0,0,0,8,0,0,4,2,0],
+    [5,0,0,6,7,0,0,0,0],
+    [0,0,0,0,0,9,0,0,5],
+    [7,4,0,1,0,0,0,0,0],
+    [0,0,9,0,3,0,7,0,0],
+    [0,0,0,0,0,7,0,4,8],
+    [8,0,0,4,0,0,0,0,0],
+    [0,0,0,0,9,8,0,0,3],
+    [0,9,5,0,0,3,0,0,0]
+]
+
+def print_board(board):
+    for i, val in enumerate(board):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j, val in enumerate(board[0]):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+def open_space(board):
+    for i, row in enumerate(board):
+        for j, val in enumerate(row):
+            if val == 0:
+                return (i, j)  # row, col
+
+    return None
+
+
+def valid(board, num, pos):
+    # Check row
+    for i, val in enumerate(board[0]):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check column
+    for i, val in enumerate(board):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
+            if board[i][j] == num and (i,j) != pos:
+                return False
+
+    return True
+
+
+def solve(board):
+    find = open_space(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if valid(board, i, (row, col)):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+
+print_board(sudoku)
+solve(sudoku)
+print("")
+print("Solved:")
+print_board(sudoku)
