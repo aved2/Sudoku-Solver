a5c39fb95501c62e0b0b6e9b01538c047a3d8af7
Aditya Ved <63937430+aved2@users.noreply.github.com>
2023-08-08 01:00:07 -0700

    Update sudokuSolver.py

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -34,22 +34,24 @@ def open_space(board):
 
 
 def valid(board, num, pos):
-    # Check row
+
+    
+    # Check Row
     for i, val in enumerate(board[0]):
         if board[pos[0]][i] == num and pos[1] != i:
             return False
 
-    # Check column
+    # Check Column
     for i, val in enumerate(board):
         if board[i][pos[1]] == num and pos[0] != i:
             return False
 
-    # Check box
-    box_x = pos[1] // 3
-    box_y = pos[0] // 3
+    # Check Square
+    square_x = pos[1] // 3
+    square_y = pos[0] // 3
 
-    for i in range(box_y*3, box_y*3 + 3):
-        for j in range(box_x * 3, box_x*3 + 3):
+    for i in range(square_y*3, square_y*3 + 3):
+        for j in range(square_x * 3, square_x*3 + 3):
             if board[i][j] == num and (i,j) != pos:
                 return False
 
